4e8d13dbdf45144ad7f3113c0e2725ee2ebae761
Jon Carlson <jon@code42.com>
2014-09-19 22:38:39 -0500

    Added ChooseOne method

--- misc.go
+++ misc.go
@@ -7,10 +7,9 @@ import "os"
 import "log"
 import "code.google.com/p/go.crypto/ssh/terminal"
 
-/*
- * Returns the first non-empty, non-blank string value
- * Returns an empty string if none qualify
- */
+
+// ColesceStrings returns the first non-empty string value
+// or it returns an empty string if none qualify
 func CoalesceStrings(strs ...string) string {
     for _, s := range strs {
         if len(s) > 0 {
@@ -20,9 +19,7 @@ func CoalesceStrings(strs ...string) string {
     return ""
 }
 
-/*
- * Prompts user for input and returns their response
- */
+// Prompt prompts user for input and returns their response
 func Prompt(prompt string) string {
     fmt.Print(prompt)
     inReader := bufio.NewReader(os.Stdin)
@@ -30,11 +27,9 @@ func Prompt(prompt string) string {
     return text
 }
 
-/*
- * Returns true if user answered something that starts with "y".
- * Returns false if user answers something that starts with "n".
- * Returns yesIsDefault if no answer given.
- */
+// PromptYesNo returns true if user answered something that starts with "y".
+// PromptYesNo returns false if user answers something that starts with "n".
+// PromptYesNo returns yesIsDefault if no answer given.
 func PromptYesNo(prompt string, yesIsDefault bool) bool {
     for {
         if yesIsDefault {
@@ -45,7 +40,8 @@ func PromptYesNo(prompt string, yesIsDefault bool) bool {
 
         // Read input
         inReader := bufio.NewReader(os.Stdin)
-        text, _ := inReader.ReadString('\n')
+        text, err := inReader.ReadString('\n')
+		check(err, "reading stdin")
 
         // Trim leading and trailing spaces, and convert to lower case
         text = strings.TrimSpace(strings.ToLower(text))
@@ -60,9 +56,34 @@ func PromptYesNo(prompt string, yesIsDefault bool) bool {
     }
 }
 
-/*
- * Prompts user for a password that is never echoed back to the screen.
- */
+
+// ChooseOne prompts the user to enter one of n values
+// Example: answer := misc.PromptOne("Continue, Quit, or Redo? [cqr]: ", ["c", "q", "r"])
+func ChooseOne(prompt string, values ...string) string {
+    for {
+        fmt.Print(prompt)
+
+        // Read input
+        inReader := bufio.NewReader(os.Stdin)
+        text, _ := inReader.ReadString('\n')
+
+        // Trim leading and trailing spaces, and convert to lower case
+        text = strings.TrimSpace(text)
+        if len(text) == 0 {
+            return values[0]
+        }
+
+        for _, v := range values {
+            if text == v {
+                return v
+            }
+        }
+
+        fmt.Println("Invalid entry, try again.")
+    }
+}
+
+// PromptPassword prompts user for a password that is never echoed back to the screen.
 func PromptPassword(prompt string) string {
     stdin := 1
     fmt.Fprintf(os.Stdin, prompt)
